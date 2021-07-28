c09353274d46730f69807b90a91c0cb8aff7f095
Soin Roman <61582942+SoinRoma@users.noreply.github.com>
2021-07-29 01:22:56 +0500

    Update BlogFactory.php

--- database/factories/BlogFactory.php
+++ database/factories/BlogFactory.php
@@ -7,13 +7,15 @@ use Faker\Generator as Faker;
 
 $factory->define(Blog::class, function (Faker $faker) {
 
-    $title = $faker->realText(rand(10,40));
+    $title = $faker->realText(rand(10,25));
     $short_title =mb_strlen($title)>30 ? mb_substr($title, 0, 30) . '...' : $title;
     $created =$faker->dateTimeBetween('-30 days', '-1 days');
+    
     return [
         'title'=>$title,
         'short_title'=>$short_title,
         'author_id'=>rand(1,4),
+        'img' => $faker->image('public/img/images',640,480, null, false),
         'description'=>$faker->realText(rand(100,500)),
         'created_at'=>$created,
         'updated_at'=>$created,
